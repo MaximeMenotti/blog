0f196bf35942f12b9fcff9662d5d4762d542d00d
Maxime Menotti <maxime.menotti@gmail.com>
2020-03-30 09:33:54 +0200

    :rotating_light: Remove warning

--- frontend/src/hooks/useOnTick.ts
+++ frontend/src/hooks/useOnTick.ts
@@ -1,8 +1,7 @@
 import { useEffect } from 'react';
 
-function useOnTick(callback: Function, timeout: number = 5000) {
+function useOnTick(callback: FunctionStringCallback, timeout: number = 5000) {
   useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/no-implied-eval
     const interval = setInterval(callback, timeout);
     return () => {
       clearInterval(interval);
