929ef56276d6bf2123c6cfb86205a550805b5033
Maxime Menotti <maxime.menotti@gmail.com>
2020-04-17 12:47:23 +0200

    :lipstick: Add animation and unmissable description

--- frontend/src/components/Unmissables/UnmissableTeaser.tsx
+++ frontend/src/components/Unmissables/UnmissableTeaser.tsx
@@ -1,4 +1,5 @@
-import * as React from 'react';
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
 import { Unmissable } from './Unmissable';
 import halfSplit from '../../helpers/strings';
 
@@ -8,34 +9,64 @@ export type PlaceTeaserProps = {
 };
 
 function UnmissableTeaser({ unmissable }: PlaceTeaserProps) {
-  const { first: unmissableTitleSplitFirst, second: unmissableTitleSplitSecond } = halfSplit(unmissable.title);
+  const [active, setActive] = useState(false);
+  const {
+    first: unmissableTitleSplitFirst,
+    second: unmissableTitleSplitSecond,
+  } = halfSplit(unmissable.title);
+
   let iconClassName;
 
   switch (unmissable.type) {
     case 'drink':
-      iconClassName = 'fas fa-glass-cheers';
+      iconClassName = 'title-icon fas fa-glass-cheers';
       break;
     case 'party':
-      iconClassName = 'fas fa-cocktail';
+      iconClassName = 'title-icon fas fa-cocktail';
       break;
     case 'visit':
-      iconClassName = 'fas fa-landmark';
+      iconClassName = 'title-icon fas fa-landmark';
       break;
     case 'housing':
-      iconClassName = 'fas fa-home';
+      iconClassName = 'title-icon fas fa-home';
       break;
     default:
-      iconClassName = 'fas fa-utensils';
+      iconClassName = 'title-icon fas fa-utensils';
   }
 
   return (
     <div
-      className="unmissable-teaser restaurant"
-      style={{ backgroundImage: `url(${process.env.REACT_APP_BACKEND_URL + unmissable.teaser.url})` }}
+      className={`unmissable-teaser ${active ? ' active' : ''}`}
+      onClick={() => {
+        if (!active) {
+          setActive(true);
+        }
+      }}
+      role="button"
+      tabIndex={0}
+      onKeyPress={() => {}}
     >
-      <i className={iconClassName} />
-      <span className="half-unmissable-title first">{unmissableTitleSplitFirst}</span>
-      <span className="half-unmissable-title second">{unmissableTitleSplitSecond}</span>
+      <div
+        className="unmissable-teaser-background"
+        style={{ backgroundImage: `url(${process.env.REACT_APP_BACKEND_URL + unmissable.teaser.url})` }}
+      >
+        <div
+          role="button"
+          tabIndex={0}
+          onKeyPress={() => {}}
+          className="close-icon"
+          onClick={() => { setActive(false); }}
+        >
+          <i className="fas fa-times" />
+        </div>
+        <i className={iconClassName} />
+        <span className="half-unmissable-title first">{unmissableTitleSplitFirst}</span>
+        <span className="half-unmissable-title second">{unmissableTitleSplitSecond}</span>
+      </div>
+      {
+        unmissable.description && unmissable.description !== ''
+        && <ReactMarkdown className="unmissable-description" source={unmissable.description} />
+      }
     </div>
   );
 }
